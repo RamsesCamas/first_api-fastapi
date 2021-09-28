713bc037a00f5bebb5f3235965915989b6b2eb20
Ramses Camas <machiniram@gmail.com>
2021-09-28 01:20:00 -0500

    Create API for reviews

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ from database import UserReview
 from schemas import UserRequestModel
 from schemas import UserResponseModel
 
+from schemas import ReviewRequestModel
+from schemas import ReviewResponseModel
+from schemas import ReviewRequestPutModel
+
+from schemas import MovieRequestModel
+from schemas import MovieResponseModel
+
+from typing import List
+
 
 
 app = FastAPI(title='Proyecto para pelis',
@@ -49,3 +58,60 @@ async def create_user(user: UserRequestModel):
         password = hash_password
     )
     return user
+
+@app.post('/movies', response_model=MovieResponseModel)
+async def create_movie(movie: MovieRequestModel):
+    movie = Movie.create(
+        title = movie.title
+    )
+    return movie
+
+@app.post('/reviews',response_model=ReviewResponseModel)
+async def create_review(user_review: ReviewRequestModel):
+
+    if User.select().where(User.id == user_review.user_id).first() is None:
+        raise HTTPException(status_code=404, detail='User Not Found')
+
+    if Movie.select().where(Movie.id == user_review.movie_id).first() is None:
+        return HTTPException(status_code=404,detail='Movie Not Found')
+    user_review = UserReview.create(
+        user_id=user_review.user_id,
+        movie_id=user_review.movie_id,
+        review=user_review.review,
+        score=user_review.score
+    )
+    return user_review
+
+@app.get('/reviews', response_model=List[ReviewResponseModel])
+async def get_reviews():
+    reviews = UserReview.select()
+    my_reviews = [{'id': user_re.id,'movie_id':user_re.movie_id,'review':user_re.reviews,'score':user_re.score}  for user_re in reviews]
+    return  my_reviews
+
+@app.get('/reviews/{review_id}', response_model=ReviewResponseModel)
+async def get_review(review_id:int):
+    user_review = UserReview.select().where(UserReview.id == review_id).first()
+    if user_review is None:
+        raise HTTPException(status_code=404,detail='Review Not Found')
+    return {'id': user_review.id,'movie_id':user_review.movie_id,'review':user_review.reviews,'score':user_review.score}
+
+
+@app.put('/reviews/{review_id}',response_model=ReviewResponseModel)
+async def update_review(review_id:int,review_request: ReviewRequestPutModel):
+    user_review = UserReview.select().where(UserReview.id == review_id).first()
+    if user_review is None:
+        raise HTTPException(status_code=404,detail='Review Not Found')
+    
+    user_review.review = review_request.review
+    user_review.score = review_request.score
+    user_review.save()
+    return user_review
+
+@app.delete('/reviews/{review_id}',response_model=ReviewResponseModel)
+async def delete_review(review_id:int):
+    user_review = UserReview.select().where(UserReview.id == review_id).first()
+    if user_review is None:
+        raise HTTPException(status_code=404,detail='Review Not Found')
+    user_review.delete_instance()
+
+    return {'id': user_review.id,'movie_id':user_review.movie_id,'review':user_review.reviews,'score':user_review.score}
--- schemas.py
+++ schemas.py
@@ -4,6 +4,7 @@ from pydantic import validator
 from pydantic.utils import GetterDict
 from typing import Any 
 
+
 class PeeweeGetterDict(GetterDict):
     def get(self, key: Any, default: Any = None):
         res = getattr(self._obj,key,default)
@@ -11,25 +12,50 @@ class PeeweeGetterDict(GetterDict):
             return list(res)
         return res 
 
-
-class UserRequestModel(BaseModel):
-    username: str
-    password: str
-
+class ReviewValidator():
     @validator('username')
     def username_validator(cls,username):
         if len(username) < 3 or len(username) > 50:
             raise ValueError('El nombre de usuario debe tener más de 3 caracteres y menos de 50')
         return username
-
-class UserResponseModel(BaseModel):
-    id: int
-    username: str
-
+class ResponseModel(BaseModel):
     class Config:
         orm_mode = True
         getter_dict = PeeweeGetterDict
 
+class UserRequestModel(BaseModel):
+    username: str
+    password: str
 
-class MovieBaseModel(BaseModel):
+    
+
+
+class UserResponseModel(ResponseModel):
+    id: int
+    username: str
+
+
+
+class MovieRequestModel(BaseModel):
     title: str
+
+
+class MovieResponseModel(ResponseModel):
+    id: int
+    title: str
+    
+
+class ReviewRequestModel(BaseModel,ReviewValidator):
+    user_id : int
+    movie_id: int
+    review: str
+    score: int
+class ReviewResponseModel(ResponseModel):
+    id: int
+    movie_id: int
+    review: str
+    score: int
+
+class ReviewRequestPutModel(BaseModel, ReviewValidator):
+    review: str
+    score: int
